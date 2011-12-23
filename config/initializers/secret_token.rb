8d7cb37d2874805d4bd7703dbaf5cd854e7755c3
gamtoggi <gamtoggi@gmail.com>
2011-12-23 11:51:17 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fishcake::Application.config.secret_token = '0ab79876cf097ccfe72016e23afb644b859bf411da08d555f8ecff60a363dbefb25a868f6a756e38c43e0d4245952b33857a3f95ab0c088225249c3fcb4979c3'
