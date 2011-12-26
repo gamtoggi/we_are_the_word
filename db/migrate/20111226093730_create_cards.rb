ac414e2feaad0128ba6dd242ab1c8b8c551d7780
gamtoggi <gamtoggi@gmail.com>
2011-12-26 20:30:58 +0900

    card migration 파일 null false등 지정

--- db/migrate/20111226093730_create_cards.rb
+++ db/migrate/20111226093730_create_cards.rb
@@ -1,10 +1,10 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
-      t.belongs_to :user
-      t.string :word
-      t.string :mean
-      t.integer :level
+      t.belongs_to :user, null: false
+      t.string :word, null: false
+      t.string :mean, null: false
+      t.integer :level, null: false, default: 0
 
       t.timestamps
     end
